9a9e8486f883c0e68140fbb655187d3464c99628
robynp108 <robynp108@gmail.com>
2020-05-19 16:57:19 -0500

    Updates

---
+++ precache-manifest.98fa8caf8702e79c0e9bc7204e55f794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6f8bfb0687964b84b7f26e43995f7f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dbd2e6075cb549ee2f6a",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e7365dda37efbd38ccc",
+    "url": "/react-portfolio/static/js/2.03cad01a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.03cad01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd2e6075cb549ee2f6a",
+    "url": "/react-portfolio/static/js/main.0ad1c905.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
