c0c2aea2a194b3a487bc3ae2fe63437e600d3af9
robynp108 <robynp108@gmail.com>
2020-05-19 18:11:06 -0500

    header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+// import Home from "./components/pages/Home";
+// import Portfiolio from "./components/pages/Portfolio";
+// import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        {/* <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} /> */}
+      </div>
+    </Router>
   );
 }
 
